0f1743293911c5596ce36dcf4ae74df5dc919d76
Паутов Леонид <pautovlv@gmail.com>
2015-09-11 09:46:12 +0300

    Поменял картинку при скачивании Git.

--- tests/AutoClick/Sikuli/Git.sikuli/Git.py
+++ tests/AutoClick/Sikuli/Git.sikuli/Git.py
@@ -1,4 +1,4 @@
-if exists(Pattern("DownloadsFor.png").similar(0.63),15):
-    click(Pattern("DownloadsFor.png").similar(0.63))
+if exists("4FEWindox.png",15):
+    click("4FEWindox.png")
     exit(0)
 exit(1)
