36cf95ec53c4075cdbf33e0d2ab2fdc11dec8148
Pautov Leonid <pautovlv@gmail.com>
2016-02-11 00:31:02 +0300

    Фиксирую промежуточный результат по задаче "Помощь контрибьютеру".

--- tools/CheckBehavior.cmd
+++ tools/CheckBehavior.cmd
@@ -1,6 +1,2 @@
-@echo off
 
-@echo sync the service base to src
-
-oscript .\tools\onescript\unload-service-conf.os
-oscript .\tools\onescript\run-behavior-check-session.os
+oscript .\onescript\run-behavior-check-session.os .\JSON\Main.json
