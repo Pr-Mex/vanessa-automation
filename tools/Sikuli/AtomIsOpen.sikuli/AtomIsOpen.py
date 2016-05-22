ae7dd8d5f70a0462b107f011f86b3a1b2393f21b
Паутов Леонид <pautovlv@gmail.com>
2016-05-22 20:39:49 +0300

    CIFix

--- tools/Sikuli/AtomIsOpen.sikuli/AtomIsOpen.py
+++ tools/Sikuli/AtomIsOpen.sikuli/AtomIsOpen.py
@@ -6,10 +6,11 @@ click(Pattern("1463230028949.png").similar(0.80).targetOffset(10,0))
 type("n",KeyModifier.CTRL)
 
 click("Edit.png")
+wait("SelectGramma.png",30)
 click("SelectGramma.png")
 
 sleep(2)
-type("Gherkin-ru")
+paste("Gherkin-ru")
 sleep(1)
 type(Key.ENTER)
 exit(0)
