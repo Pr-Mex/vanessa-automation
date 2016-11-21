da0f76c81c6b0ca4c215380bb8e2e3dce7a574ec
Паутов Леонид <pautovlv@gmail.com>
2016-11-21 22:46:39 +0300

    Закоментировал устаревшие команды.

--- tools/1 PrepareCheck.cmd
+++ tools/1 PrepareCheck.cmd
@@ -2,17 +2,17 @@
 
 @echo sync and update precommit1c hook
 
-git submodule update --init --recursive .\..\vendor\precommit1c 
-cd .\..\vendor\precommit1c\
+rem git submodule update --init --recursive .\..\vendor\precommit1c 
+rem cd .\..\vendor\precommit1c\
 
-xcopy .\ibService .\..\..\.git\hooks\ibService\ /Y /E /F
-xcopy .\pre-commit .\..\..\.git\hooks\ /Y /F
-xcopy .\v8Reader .\..\..\.git\hooks\v8Reader\ /Y /F
-xcopy .\pyv8unpack.py .\..\..\.git\hooks\ /Y /F
+rem xcopy .\ibService .\..\..\.git\hooks\ibService\ /Y /E /F
+rem xcopy .\pre-commit .\..\..\.git\hooks\ /Y /F
+rem xcopy .\v8Reader .\..\..\.git\hooks\v8Reader\ /Y /F
+rem xcopy .\pyv8unpack.py .\..\..\.git\hooks\ /Y /F
 
 
 @echo set the russian file support 
-cd .\..\..\
+cd .\..\
 git config --local core.quotepath false
 
 @echo prepare contibiution environ
