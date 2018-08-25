155acb5c687170327b122a957c0f4818fca22855
Pautov Leonid <pautovlv@gmail.com>
2018-08-25 12:51:47 +0300

    Обновлен скрипт

--- tools/Sikuli/WaitForFeatureLoadInEditor.sikuli/WaitForFeatureLoadInEditor.py
+++ tools/Sikuli/WaitForFeatureLoadInEditor.sikuli/WaitForFeatureLoadInEditor.py
@@ -1,2 +1,8 @@
-wait("1532881364738.png")
-exit(0)
+n = 6
+for x in range(n):
+    if exists("1533560273739.png",1):
+        exit(0)
+    if exists("1532881364738.png",1):
+        exit(0)
+
+exit(1)
