a0716c9e8e892bd17d99be446f277a0cdb528e7d
Pautov Leonid <pautovlv@gmail.com>
2019-06-01 19:44:53 +0300

    Обновлен скрипит.

--- tools/Sikuli/HotKey.sikuli/HotKey.py
+++ tools/Sikuli/HotKey.sikuli/HotKey.py
@@ -1,5 +1,32 @@
 HotKey = sys.argv[1]
 Modif = sys.argv[2] 
+
+lowerHotKey = str.lower(HotKey.encode('utf-8'))
+if lowerHotKey == 'f1':
+    HotKey = Key.F1
+elif lowerHotKey == 'f2':
+    HotKey = Key.F2
+elif lowerHotKey == 'f3':
+    HotKey = Key.F3
+elif lowerHotKey == 'f4':
+    HotKey = Key.F4
+elif lowerHotKey == 'f5':
+    HotKey = Key.F5
+elif lowerHotKey == 'f6':
+    HotKey = Key.F6
+elif lowerHotKey == 'f7':
+    HotKey = Key.F7
+elif lowerHotKey == 'f8':
+    HotKey = Key.F8
+elif lowerHotKey == 'f9':
+    HotKey = Key.F9
+elif lowerHotKey == 'f10':
+    HotKey = Key.F10
+elif lowerHotKey == 'f11':
+    HotKey = Key.F11
+elif lowerHotKey == 'f12':
+    HotKey = Key.F12
+
 if Modif == 'win':
     type(HotKey, KeyModifier.WIN)
 elif Modif == 'ctrl':
