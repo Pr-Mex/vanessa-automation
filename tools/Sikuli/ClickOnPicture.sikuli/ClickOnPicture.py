5daf5a4f501e36f6523353182f30adec0597e361
Dmitry Sherstobitov <Lex@dineva.net>
2020-07-05 07:46:19 -0700

    resolve #854

--- tools/Sikuli/ClickOnPicture.sikuli/ClickOnPicture.py
+++ tools/Sikuli/ClickOnPicture.sikuli/ClickOnPicture.py
@@ -4,18 +4,18 @@ Find = 0
 if Find == 0:
     if exists(Pattern(PictureToClick).similar(0.70)):
         click(Pattern(PictureToClick).similar(0.70))
-	    Find = 1
+        Find = 1
 
 
 if Find == 0:
     if exists(Pattern(PictureToClick).similar(0.60)):
         click(Pattern(PictureToClick).similar(0.60))
-	    Find = 1
+        Find = 1
 
 
 if Find == 0:
     if exists(Pattern(PictureToClick).similar(0.50)):
         click(Pattern(PictureToClick).similar(0.50))
-	    Find = 1
+        Find = 1
 
 exit(0)
