9994b4ad6075bdced5b9a0923a71558b3a411ec5
Крапивин Андрей Викторович <a.krapivin@skbkontur.ru>
2022-01-13 09:34:32 +0500

    feat: Сделал парсер всех текстов сразу

--- tools/python/grammar/check.py
+++ tools/python/grammar/check.py
@@ -6,6 +6,18 @@ namespaces = {'logform': 'http://v8.1c.ru/8.3/xcf/logform', 'core': 'http://v8.1
 et.register_namespace('', 'http://v8.1c.ru/8.3/MDClasses')
 
 
+def getRuContent(element: et.Element)-> str:
+    if element is None:
+        return None
+    items = element.findall('core:item', namespaces)
+    for item in items:
+        lang = item.find('core:lang', namespaces).text
+        if lang != 'ru':
+            continue
+        content = item.find('core:content', namespaces).text
+        return content
+
+
 def getChildItems(item: et.Element)-> dict:
     result = {}
     all_elements = item.find('logform:ChildItems', namespaces)
@@ -13,13 +25,22 @@ def getChildItems(item: et.Element)-> dict:
         return result
     for element in all_elements:
         name = element.attrib.get('name')
-        tooltips = element.findall('logform:ExtendedTooltip/logform:Title/*', namespaces)
-        for tooltip in tooltips:
-            lang = tooltip.find('{http://v8.1c.ru/8.1/data/core}lang').text
-            if lang != 'ru':
-                continue
-            content = tooltip.find('{http://v8.1c.ru/8.1/data/core}content').text
-            result[name] = content
+
+        title = element.find('logform:Title', namespaces)
+        content = getRuContent(title)
+        if content is not None:
+            result[f"{name}.Заголовок"] = content
+
+        tooltip = element.find('logform:Tooltip', namespaces)
+        content = getRuContent(tooltip)
+        if content is not None:
+            result[f"{name}.Подсказка"] = content
+
+        ext_tooltip = element.find('logform:ExtendedTooltip/logform:Title', namespaces)
+        content = getRuContent(ext_tooltip)
+        if content is not None:
+            result[f"{name}.РасшПодсказка"] = content
+
         result.update(getChildItems(element))
     return result
 
